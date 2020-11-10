8347b71a12fc1bc6659a3398379e5d335079f4d0
Steven Guccione <Steven.Guccione@gmail.com>
2020-11-10 16:02:09 -0600

    Minor code cleanup.

--- logger.py
+++ logger.py
@@ -18,16 +18,21 @@ The output to the log file will be of the form:
 
 Logs curently rotate every 24 hours at midnight and are kept for 31 days.
 
+Version 1.0:  Sept. 3, 2020: Initial version
+Version 1.1:  Nov. 5, 2020:  Cleaned up.  Updated subprocess.
+
 """
 
 __author__    = "Steven A. Guccione"
-__date__      = "September 3, 2020"
+__date__      = "November 5, 2020"
 __copyright__ = "Copyright (c) 2020 by Steven A. Guccione"
+__version__   = "1.1"
 
 import argparse
-import datetime
 import time
 import subprocess
+import os
+import sys
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
@@ -35,13 +40,14 @@ if __name__ == '__main__':
 
    # Parse command line parameters
    parser = argparse.ArgumentParser()
-   parser.add_argument("--command", required=True, help="command")
+   parser.add_argument("--command", required=True, help="command to be executed")
    parser.add_argument("--filename", default="log", help="logfile name (default=log)")
-   parser.add_argument("--postprocess", help="python post-process command")
+   parser.add_argument("--postprocess", help="python string post-process command")
    parser.add_argument("--delay", type=int, help="delay in seconds (default=60)", default=60)
    parser.add_argument("--debug", help="debug flag", action='store_true')
    args = parser.parse_args()
 
+   # Set up Logging   
    logger = logging.getLogger('loggerlog')
    if args.debug:
       logger.setLevel(logging.DEBUG)
@@ -55,22 +61,14 @@ if __name__ == '__main__':
    logger.addHandler(logHandler)
    
    try:
-      while True:
 
-         # Get current time for timestamp
-         current_time = datetime.datetime.today()
-         if args.debug:
-            print(current_time)
+      # Loop forever
+      while True:
 
          try:
              
             # Run command
-            p = subprocess.Popen(args.command, shell=True,
-                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            out, err = p.communicate()
-
-            # Get the command output
-            cmd_out = out.decode("ASCII")
+            cmd_out = subprocess.check_output(args.command, shell=True).decode()
             if args.debug:
                logger.debug(cmd_out)
             if args.postprocess:
@@ -81,17 +79,22 @@ if __name__ == '__main__':
          except Exception as e:
             # Something went wrong executing command
             logger.error(e)
-            
-         logger.info(cmd_out)
+            cmd_out = ""
 
-         # Total delay will include time to ping
+         # Print to log
+         if cmd_out:
+            logger.info(cmd_out)
+
+         # Delay (addition to time to run comand)
          time.sleep(args.delay)
 
    # Exit on CTRL-C
    except KeyboardInterrupt:
       logger.error("Execution interrupted.  Exiting.")
-   # Any other error
+   # Any other unexpected error
    except Exception as e:
       logger.error(e)
-   
-   SystemExit(0)
+
+   # Should never get here
+   logger.error("Unkown error.  Main loop exited.")
+   sys.exit(1)
